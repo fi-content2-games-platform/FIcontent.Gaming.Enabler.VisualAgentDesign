029c37c3ac77552f81c0d3c8cb643c5b1b19c956
Stéphane Magnenat <stephane@magnenat.net>
2014-09-01 16:37:24 +0200

    Added timeline on code side.

--- LiveInspector/Assets/Scripts/AsebaListener.cs
+++ LiveInspector/Assets/Scripts/AsebaListener.cs
@@ -4,13 +4,81 @@ using System.Collections.Generic;
 using System;
 using Aseba;
 
-public class AsebaListener : MonoBehaviour {
+public class Timeline
+{
+	const float minTimeForContinuous = 0.2f;
+	
+	public struct Stride
+	{
+		public float startTime, endTime;
+		
+		public Stride(float startTime, float endTime)
+		{
+			this.startTime = startTime;
+			this.endTime = endTime;
+		}
+	};
+	
+	Dictionary<ushort, List<Stride>> pastEvents = new Dictionary<ushort, List<Stride>>();
+	
+	Dictionary<ushort, Stride> ongoingEvents = new Dictionary<ushort, Stride>();
+	
+	// Push an event, return whether it created a new stride
+	public bool PushEvent(ushort setId)
+	{
+		// TODO: use timer to commit old events even though we did not receive new ones
+		// see whether we have uncommited outdated strides
+		List<ushort> toRemove = new List<ushort>();
+		foreach (KeyValuePair<ushort, Stride> entry in ongoingEvents)
+		{
+			if (entry.Value.endTime + minTimeForContinuous < Time.time)
+			{
+				pastEvents[entry.Key] = new List<Stride> { entry.Value };
+				toRemove.Add(entry.Key);
+			}
+		}
+		foreach (ushort key in toRemove)
+		{
+			ongoingEvents.Remove(key);
+			Debug.Log(String.Format("Removing set {0}", key));
+		}
+		
+		// check if already ongoing, and update time or add
+		bool newStride;
+		Stride stride;
+		if (ongoingEvents.TryGetValue(setId, out stride))
+		{
+			ongoingEvents[setId] = new Stride(stride.startTime, Time.time);
+			newStride = false;
+		}
+		else
+		{
+			ongoingEvents[setId] = new Stride(Time.time, Time.time);
+			newStride = true;
+		}
+		
+		// TODO: call delegates if any
+		
+		return newStride;
+	}
+	
+	public void Clear()
+	{
+		pastEvents.Clear();
+		ongoingEvents.Clear();
+	}
+};
+
+public class AsebaListener : MonoBehaviour
+{
 
 	public Aseba.Stream stream = null;
 
 	public EventActionsSetPanel panelPrefab;
 	
 	Dictionary<ushort, Texture2D> eventActionsSetTextureCache = new Dictionary<ushort, Texture2D>();
+	
+	Timeline timeline = new Timeline();
 	
 	// Connect to the Aseba network
 	void Start ()
@@ -45,6 +113,7 @@ public class AsebaListener : MonoBehaviour {
 		{
 			//Debug.Log("New code, clearing cache");
 			eventActionsSetTextureCache.Clear();
+			timeline.Clear();
 			return;
 		}
 
@@ -57,19 +126,24 @@ public class AsebaListener : MonoBehaviour {
 		for (int i=0; i<len; i += 2)
 			wordPayload[i/2] = BitConverter.ToUInt16(payload, i);
 		
-		// create texture if needed
-		Texture2D tex;
+		// get set ID, see whether we have to show it
 		ushort setId = wordPayload[0];
-		if (!eventActionsSetTextureCache.ContainsKey(setId))
-			tex = eventActionsSetTextureCache[setId] = VPL.CreateTexture(wordPayload);
-		else
-			tex = eventActionsSetTextureCache[setId];
+		if (timeline.PushEvent(setId))
+		{
+			// create texture if needed
+			Texture2D tex;
+			if (!eventActionsSetTextureCache.ContainsKey(setId))
+				tex = eventActionsSetTextureCache[setId] = VPL.CreateTexture(wordPayload);
+			else
+				tex = eventActionsSetTextureCache[setId];
+			
+			// instanciate panel	
+			EventActionsSetPanel panel = (EventActionsSetPanel) Instantiate(panelPrefab, transform.position, transform.rotation);
+			panel.renderer.material.mainTexture = tex;
+			const float scale = 0.05f / 256;
+			panel.transform.localScale = new Vector3(tex.width * scale, tex.height * scale, 0);
+		}
 		
-		// instanciate panel	
-		EventActionsSetPanel panel = (EventActionsSetPanel) Instantiate(panelPrefab, transform.position, transform.rotation);
-		panel.renderer.material.mainTexture = tex;
-		const float scale = 0.05f / 256;
-		panel.transform.localScale = new Vector3(tex.width * scale, tex.height * scale, 0);
 		// log data
 		//Debug.Log(String.Format("Received VPL event from {0} containing {1} words : {2}", source, wordPayload.Length, String.Join(", ", Array.ConvertAll<ushort, string>(wordPayload, Convert.ToString))));
 	}
